951137f17b847e064d6cd7a67af7c737607e3f75
sysmoai <sysmoai.com@gmail.com>
2026-08-10 08:25:12 +0600

    a11y(app): load accessibility guardrails after base styles

--- artifacts/aips-landing/src/main.tsx
+++ artifacts/aips-landing/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import RootApp from "./RootApp";
 import "./index.css";
+import "./accessibility.css";
 
 createRoot(document.getElementById("root")!).render(<RootApp />);

acdf3ae171f74d76abe3f05715f7555fa6c8ec7e
sysmoai <sysmoai.com@gmail.com>
2026-08-10 06:42:35 +0600

    feat(plans): route dedicated plan views through safe root app

--- artifacts/aips-landing/src/main.tsx
+++ artifacts/aips-landing/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import RootApp from "./RootApp";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<RootApp />);
